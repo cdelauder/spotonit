8961c1fa31cd781cc2f956a7a31dd809a759654d
cdelauder <cdelauder@gmail.com>
2014-09-03 16:41:21 -0700

    cant find the results html file for some reason. will probably switch to something I can pass js anyway such as jade. then I can send the user back with the relevant data, which will be easier.

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ var server = express()
 
 server.get('/', function (request, response) {
   response.set('Content-Type', 'text/html');
-  response.sendfile(__dirname + '/index.html')
+  response.sendFile(__dirname + '/index.html')
+})
+
+server.post('/crawl', function (request, response) {
+  console.log('in post route')
+  response.redirect('/results')
+})
+
+server.get('/results', function (request, response) {
+  response.set('Content-Type', 'text/html')
+  response.sendFile(__dirname, + '/results.html')
 })
 
 server.listen(3000)
