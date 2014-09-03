65489fb14d4e78ee1ee7564744ea173ba8a226af
cdelauder <cdelauder@gmail.com>
2014-09-03 16:57:13 -0700

    added pseudocode now that I have been working on this for an hour and understand the problem a little better. also added jade. need to convert the html to jade.

---
+++ crawler.js
@@ -0,0 +1,7 @@
+exports.crawler = function (url) {
+  // make request with the url
+  // pull the links from the response and filter any links that don't include the word events
+    // if this is not efficient enough pull the url that has the word event and no numbers
+    // pull the links from the response and return any that include /(event)+.+\d+/
+  // return the links
+} 
--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 var express = require('express')
 var server = express()
 
+
 server.get('/', function (request, response) {
   response.set('Content-Type', 'text/html');
   response.sendFile(__dirname + '/index.html')
 })
 
 server.post('/crawl', function (request, response) {
-  console.log('in post route')
+  // make crawler instance
+  // pass url from request
+  // redirect to '/' with result of the crawler module
   response.redirect('/results')
 })
 
-server.get('/results', function (request, response) {
-  response.set('Content-Type', 'text/html')
-  response.sendFile(__dirname, + '/results.html')
-})
 
 server.listen(3000)
