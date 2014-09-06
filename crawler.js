c0697362e3788a5253408c575cc9b723b60db98a
cdelauder <cdelauder@gmail.com>
2014-09-06 16:44:49 -0700

    getting close. I am getting the correct result for 50% of the sites. By adding a loop to iterate throught the urls I get from the first page I will be able to get enough links for the 3rd site. The workshop site will be really hard as that is all query strings.

--- crawler.js
+++ crawler.js
@@ -1,27 +1,67 @@
 
   // make request with the url
-  // pull the links from the response and filter any links that don't include the word events
+  // pull the links from the response as long as they include the word events
     // if this is not efficient enough pull the url that has the word event and no numbers
+      //definitely need to go to the events page
+      //because a restful site would follow events/:id formula, so no numbers should mean an index page which lists all events
     // pull the links from the response and return any that include /(event)+.+\d+/
   // return the links
 
 exports.begin = function (url) {
 
   var http = require('http')
+  var parser = require('url')
 
-  var body = ''
-  
-  http.get(url, function (res) {
-    console.log(res.statusCode)
+  var eventsRegex = /<a href="(\S*?event\S*?\/\d*\S*?)"/ig
+  var eventPageRegex = /<a href="(\S*?event\S*?)"/ig
+
+  var body = []
+  var links = []
+  var host = parser.parse(url, false, true)['host']
+
+
+  var req = http.get(url, function (res) {
+    // call the input url
     res.on('data', function (chunk) {
-      body += chunk.toString()
-      // var links = body.match(/(http.*event.*")*/)
-      console.log(body)
+      var match = eventPageRegex.exec(chunk.toString())
+      // if we find a url that we expect to be a link to the event #show page add it to the our array
+      if (match) {
+        body.push(match[1])
+      }
     })
+    res.on('end', function () {
+      //once we have all the data, make a second request to get the show page
+      console.log('body ' + body)
+      var eventUrl = body[0]
+      //make sure the url is properly formatted and includes the host name
+
+      if ( eventUrl.indexOf(host) === -1) {
+        eventUrl = 'http://' + host + eventUrl
+      }
+
+      //make the request
+      console.log('url ' + eventUrl)
+      if (eventUrl) {
+        http.get(eventUrl, function (res) {
+          res.on('data', function (chunk) {
+            //save any links that match the regex format which should indicate an event#show page
+            var match = eventsRegex.exec(chunk.toString())
+            if (match) {
+              links.push(match[1])
+            }
+          })
+          res.on('end', function () {
+            console.log('links ' + links)
+          })
+        }).on('error', function (e) {
+          console.log(e.message)
+        })
+      }
+    })
+
+
   }).on('error', function (e) {
-    console.log(e)
+    console.log(e.message)
   })
-
-
 
 }
